10fc07186d03d7015ac5db63fd6bce950d000653
Tom Lebreux <tom.lebreux@suse.com>
2025-02-12 11:24:58 -0500

    fixup! Add browser-side pagination watch mechanism

--- pkg/stores/sqlproxy/proxy_store.go
+++ pkg/stores/sqlproxy/proxy_store.go
@@ -622,7 +622,7 @@ func (s *Store) watch(apiOp *types.APIRequest, schema *types.APISchema, w types.
 	tableClient := &tablelistconvert.Client{ResourceInterface: client}
 	attrs := attributes.GVK(schema)
 	ns := attributes.Namespaced(schema)
-	inf, err := s.cacheFactory.CacheFor(fields, transformFunc, tableClient, attrs, ns, controllerschema.IsListWatchable(schema))
+	inf, err := s.cacheFactory.CacheFor(s.ctx, fields, transformFunc, tableClient, attrs, ns, controllerschema.IsListWatchable(schema))
 	if err != nil {
 		return nil, err
 	}
